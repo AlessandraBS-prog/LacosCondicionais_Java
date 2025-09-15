e8578dab9c06349555242e548b0d676d1c8c04ff
AlessandraBS-BS <alessandratulio2017@gmail.com>
2025-09-15 20:45:41 -0300

    Create CalculadoraSimple.java

---
+++ CalculadoraSimple.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Escolha a operação:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.print("Digite a opção desejada: ");
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println("Resultado: " + (num1 + num2));
+                break;
+            case 2:
+                System.out.println("Resultado: " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Resultado: " + (num1 * num2));
+                break;
+            case 4:
+                if (num2 != 0) {
+                    System.out.println("Resultado: " + (num1 / num2));
+                } else {
+                    System.out.println("Erro: divisão por zero!");
+                }
+                break;
+            default:
+                System.out.println("Opção inválida!");
+        }
+
+        scanner.close();
+    }
+}
